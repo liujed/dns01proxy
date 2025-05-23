319071a7938d4aefb55a4d2c62064a76ff70469d
Jed Liu <liujed@users.noreply.github.com>
2025-05-23 15:45:01 -0400

    Implement `hash-password` command

--- cmd/hash_password.go
+++ cmd/hash_password.go
@@ -1,15 +1,41 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	caddycmd "github.com/caddyserver/caddy/v2/cmd"
+	"github.com/liujed/caddy-dns01proxy/flags"
+	"github.com/liujed/goutil/optionals"
+	"github.com/spf13/cobra"
+)
+
+// Flag definitions.
+var (
+	flgPlaintext = flags.Flag[string]{
+		Name:      "plaintext",
+		ShortName: optionals.Some('p'),
+		UsageMsg:  "the plaintext password",
+	}
+
+	flgAlgorithm = flags.Flag[string]{
+		Name:         "algorithm",
+		ShortName:    optionals.Some('a'),
+		DefaultValue: "bcrypt",
+		UsageMsg:     "the name of the hash algorithm",
+		Hidden:       true,
+	}
+)
 
 // The `hash-password` command.
 var hashPasswordCmd = &cobra.Command{
 	Use:   "hash-password",
 	Short: "Hashes a password and prints the result to stdout in base64",
 	Run: func(cmd *cobra.Command, args []string) {
+		caddycmd.Main()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(hashPasswordCmd)
+
+	flags.AddStringFlag(hashPasswordCmd, flgPlaintext)
+	flags.AddStringFlag(hashPasswordCmd, flgAlgorithm)
 }
